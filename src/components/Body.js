974795cbecd8d6a18aac92964c709f4c78e7dd57
Anchal Arora <aanchalarora392@gmail.com>
2024-09-04 16:49:08 +0530

    Bugfix : Swiggy API response issue fixed

--- src/components/Body.js
+++ src/components/Body.js
@@ -27,27 +27,26 @@ const Body = () => {
 
   const { setUserName, loggedInUser } = useContext(UserContext);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   const fetchData = async () => {
     try {
       const data = await fetch(SWIGGY_LIST_API);
       const json = await data.json();
-      console.log(" fetched resaturants data ", json);
-      console.log(
-        "resaturants data",
-        json.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle.restaurants
-      );
-      setRestaurants(
-        json.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle.restaurants
-      );
-      setFilteredSearchList(
-        json.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle.restaurants
-      );
-      //setRestaurants(json.meals)
-      //setFilteredSearchList(json.meals)
+
+      function checkJsonData(jsonData) {
+        for (let i = 0; i < jsonData?.data?.cards.length; i++) {
+          let checkData =
+            json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle
+              ?.restaurants;
+
+          if (checkData !== undefined) {
+            return checkData;
+          }
+        }
+      }
+      const resData = checkJsonData(json);
+
+      setRestaurants(resData);
+      setFilteredSearchList(resData);
     } catch (error) {
       console.error("Failed to fetch", error);
       setFilteredSearchList([]);
@@ -55,8 +54,19 @@ const Body = () => {
     }
   };
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+  useEffect(() => {
+    const filteredSearchedData = restaurants.filter((res) =>
+      res.info?.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredSearchList(filteredSearchedData);
+  }, [searchText]);
+
   if (onlineStatus === false) return <h1>Looks like you are offline</h1>;
 
+  console.log("restaurants?.length", restaurants?.length);
   return restaurants?.length === 0 ? (
     <Shimmer />
   ) : (
@@ -71,15 +81,16 @@ const Body = () => {
             onChange={(e) => setSearchText(e.target.value)}
           />
           <button
+            data-testid="searchButton"
             className="px-4 py-2 bg-green-500 text-white rounded-lg"
             onClick={() => {
               const filteredSearchedData = restaurants.filter((res) =>
-                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                res.info?.name.toLowerCase().includes(searchText.toLowerCase())
               );
               setFilteredSearchList(filteredSearchedData);
             }}
           >
-            Search
+            SearchBox
           </button>
         </div>
         <div className="search flex items-center space-x-4 m-4 p-4">
@@ -116,8 +127,8 @@ const Body = () => {
           filteredSearchList.map((restaurant, index) => (
             // <ChickenRestaurantCard key={restaurant.idMeal} restaurant={restaurant} />
             <Link
-              to={"/restaurants/" + restaurant.info.id}
-              key={restaurant.info.id + index}
+              to={"/restaurants/" + restaurant.info?.id}
+              key={restaurant.info?.id + index}
             >
               {restaurant.info.promoted ? (
                 <PromoteRestaurantCard restaurant={restaurant} />
