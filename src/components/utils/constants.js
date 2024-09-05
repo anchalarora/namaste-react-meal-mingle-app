121eb3b1ce1c9fa4ed26316b482d3d32e2609ff5
Anchal Arora <aanchalarora392@gmail.com>
2024-09-05 21:58:00 +0530

    Constants file updated with LOGO url

--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -10,3 +10,6 @@ export const CDN_URL =
 export const MEAL_MINGLE_MENU_API_URL_1 = `${process.env.REACT_FOODFIRE_APP_BASE_URL}menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=`;
 
 export const MEAL_MINGLE_LIST_API_1 = `${process.env.REACT_FOODFIRE_APP_BASE_URL}restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING`;
+
+export const LOGO_URL =
+  "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/delivery-logo-design-template-a27c923ba11012bd16506664a693629b_screen.jpg?ts=1694133068";
