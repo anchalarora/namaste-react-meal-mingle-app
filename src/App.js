5df5c53c5bdb6284f91c977893c49ec9ca9e09d1
Anchal Arora <aanchalarora392@gmail.com>
2024-09-06 21:00:04 +0530

    Changed how About Page loads : Lazy loading

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import About from "./components/About";
 import ContactUs from "./components/ContactUs";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
