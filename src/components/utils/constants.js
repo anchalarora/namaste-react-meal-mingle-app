0b05c3b73f9b5c59cc97091612e1732797429e50
Anchal Arora <aanchalarora392@gmail.com>
2024-09-09 23:06:08 +0530

    MENU API url uodated

--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -7,7 +7,7 @@ export const MEAL_MINGLE_LIST_API_1 =
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MEAL_MINGLE_MENU_API_URL = `${process.env.REACT_FOODFIRE_APP_BASE_URL}menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId=`;
+export const MEAL_MINGLE_MENU_API_URL = `${process.env.REACT_FOODFIRE_APP_BASE_URL}menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=`;
 
 export const MEAL_MINGLE_LIST_API = `${process.env.REACT_FOODFIRE_APP_BASE_URL}restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING`;
 
